bced3e3bafb0c57e9855020c742b71f16fe7a842
Synzenn <velriel42@gmail.com>
2024-05-28 10:49:35 +0700

    membuat drivedclass

--- Part3/Part3.cpp
+++ Part3/Part3.cpp
@@ -7,7 +7,12 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
-
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
 int main() {
-   
+    
 }
